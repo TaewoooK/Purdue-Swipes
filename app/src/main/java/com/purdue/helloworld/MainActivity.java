484136f8a60a9f09ecc2e0a07d251e0e2cbe6cc8
Samuelford24 <Samuelford48@gmail.com>
2020-10-17 11:01:28 -0400

    Initial commit

---
+++ app/src/main/java/com/purdue/helloworld/MainActivity.java
@@ -0,0 +1,183 @@
+package com.purdue.helloworld;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.purdue.helloworld.ui.dashboard.DashboardFragment;
+import com.purdue.helloworld.ui.home.HomeFragment;
+import com.purdue.helloworld.ui.notifications.NotificationsFragment;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signout2;
+    private TextView mTextMessage;
+
+    final Fragment fragment1 = new HomeFragment();
+    final Fragment fragment2 = new DashboardFragment();
+    final Fragment fragment3 = new NotificationsFragment();
+    //  final Fragment fragment4 = new Admin_fragment();
+    //  final Fragment fragment5 = new HR_fragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = fragment1;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    // mTextMessage.setText(R.string.title_home);
+                    fm.beginTransaction().hide(active).show(fragment1).commit();
+
+                    active = fragment1;
+                    fm.popBackStack();
+                    return true;
+                case R.id.navigation_dashboard:
+                    // mTextMessage.setText(R.string.title_dashboard);
+                    fm.beginTransaction().hide(active).show(fragment2).commit();
+                    active = fragment2;
+                    fm.popBackStack();
+                    return true;
+                case R.id.navigation_notifications:
+                    //mTextMessage.setText(R.string.title_notifications);
+                    fm.beginTransaction().hide(active).show(fragment3).commit();
+                    active = fragment3;
+                    fm.popBackStack();
+                    return true;
+              /*  case R.id.admin:
+                    fm.beginTransaction().hide(active).show(fragment4).commit();
+                    active = fragment4;
+                    fm.popBackStack();
+                    return true;
+                case R.id.Homeroom:
+                    fm.beginTransaction().hide(active).show(fragment5).commit();
+                    active = fragment5;
+                    fm.popBackStack();
+                    return true;*/
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //  fm.beginTransaction().add(R.id.main_container, fragment5, "5").hide(fragment5).commit();
+        //  fm.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment4).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.main_container, fragment1, "1").commit();
+
+    }
+
+  /*  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signout2 = (Button) findViewById(R.id.sign_out);
+
+        signout2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(com.samuelford48gmail.thsconnect.teacher.MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user == null) {
+                // user auth state is changed - user is null
+                // launch login activity
+                startActivity(new Intent(com.samuelford48gmail.thsconnect.teacher.MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        }
+
+
+    };
+    // public void signout (View v){
+    //    signOut();
+    //}
+/*
+    public void signOut() {
+        auth.signOut();
+
+
+// this listener will be called when there is change in firebase user session
+       FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(com.samuelford48gmail.thsconnect.teacher.MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            auth.removeAuthStateListener(authListener);
+        }
+    */
+  /*@Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu., menu);
+      return super.onCreateOptionsMenu(menu);
+  }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(com.samuelford48gmail.thsconnect.teacher.MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+*/
+}
---
+++ app/src/main/java/com/purdue/helloworld/ui/home/HomeFragment.java
@@ -0,0 +1,67 @@
+package com.purdue.helloworld.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.purdue.helloworld.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeFragment extends Fragment {
+
+String retrievedValue;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        String test = "Monday-Breakfast-8-10-Lunch-11-2-Dinner-6-8(5),Saturday-Breakfast-8-10-Lunch-11-2-Dinner-0-0";
+        //Map<String, String> map = new HashMap<>();
+       // map.put("test",test);
+        //FirebaseFirestore.getInstance().collection("Restaurants").document("Wiley").set(map);
+            FirebaseFirestore.getInstance().collection("Restaurants").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                    if (error == null) {
+                        for (DocumentSnapshot d:queryDocumentSnapshots){
+                           retrievedValue = d.get("test").toString();
+                           parseString(retrievedValue);
+                        }
+                    }
+                }
+            }
+            );
+        return root;
+    }
+
+    private void parseString(String retrievedValue) {
+        String[] split = retrievedValue.split(",");
+        System.out.println(split);
+        for (String s: split) {
+            int index = s.indexOf("-");
+            //gets day
+            String day = s.substring(0,index);
+            System.out.println(day);
+            s = s.substring(index+1);
+            int index2 = s.indexOf("-");
+            //gets breakfast
+            String meal = s.substring(0,index2);
+            System.out.println(meal);
+        }
+    }
+}
