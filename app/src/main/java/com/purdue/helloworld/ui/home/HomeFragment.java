60eaa195b3810164b205609dc8f7fba9b49c4f2e
Samuelford24 <Samuelford48@gmail.com>
2020-10-17 15:46:11 -0400

    parsing method update

--- app/src/main/java/com/purdue/helloworld/ui/home/HomeFragment.java
+++ app/src/main/java/com/purdue/helloworld/ui/home/HomeFragment.java
@@ -18,8 +18,10 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.purdue.helloworld.MealSwipeTime;
 import com.purdue.helloworld.R;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,116 +33,23 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        String test = "Monday-Breakfast-8-10-Lunch-11-2-Dinner-6-8(5),Saturday-Breakfast-8-10-Lunch-11-2-Dinner-0-0";
+      //  String test = "Monday-Breakfast-8-10-Lunch-11-2-Dinner-6-8(5),Saturday-Breakfast-8-10-Lunch-11-2-Dinner-0-0";
         //Map<String, String> map = new HashMap<>();
         // map.put("test",test);
         //FirebaseFirestore.getInstance().collection("Restaurants").document("Wiley").set(map);
-        FirebaseFirestore.getInstance().collection("Restaurants").addSnapshotListener(new EventListener<QuerySnapshot>() {
-                                                                                          @Override
-                                                                                          public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
-                                                                                              if (error == null) {
-                                                                                                  for (DocumentSnapshot d : queryDocumentSnapshots) {
-                                                                                                      retrievedValue = d.get("test").toString();
-                                                                                                      parseString(retrievedValue);
-                                                                                                  }
-                                                                                              }
-                                                                                          }
-                                                                                      }
-        );
+       // FirebaseFirestore.getInstance().collection("Restaurants").addSnapshotListener(new EventListener<QuerySnapshot>() {
+        //                                                                                  @Override
+         //                                                                                 public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+          //                                                                                    if (error == null) {
+          //                                                                                        for (DocumentSnapshot d : queryDocumentSnapshots) {
+           //                                                                                           retrievedValue = d.get("test").toString();
+            //                                                                                          parseString(retrievedValue);
+             //                                                                                     }
+             //                                                                                 }
+              //                                                                            }
+              //                                                                        }
+        //);
         return root;
     }
 
-    private void parseString(String retrievedValue) {
-        String[] split = retrievedValue.split(",");
-        String breakfast = "";
-        String breakfasthours = "";
-        String lunch = "";
-        String lunchhours = "";
-        String dinner = "";
-        String dinnerhours = "";
-        int repeat = 0;
-        System.out.println(split);
-        String test = "Monday-Breakfast-8-10-Lunch-11-2-Dinner-6-8(5),Saturday-Breakfast-8-10-Lunch-11-2-Dinner-0-0";
-        for (String s : split) {
-            int index = s.indexOf("-");
-            //gets day
-            String day = s.substring(0, index);
-            System.out.println(day);
-            s = s.substring(index + 1);
-            int index2 = s.indexOf("-");
-
-            while (index2 != -1) {
-                //gets breakfast
-                if (s.substring(0, index2).equals("Breakfast")) {
-                    //gets breakfast
-                    breakfast = s.substring(0, index2);
-                    //cuts breakfast and -
-                    s = s.substring(index2 + 1);
-                    //index of - after first time
-                    int indextemp = s.indexOf("-");
-                    //adding start time
-                    //8
-                    breakfasthours += s.substring(0, indextemp);
-                    breakfasthours += "-";
-                    s = s.substring(indextemp + 1);
-                    //getting index of dash after second time
-                    int indextemp2 = s.indexOf("-");
-                    //adding second time
-                    breakfasthours += s.substring(0, indextemp2);
-                    s = s.substring(indextemp2 + 1);
-                    index2 = s.indexOf("-");
-
-                } else if (s.substring(0, index2).equals("Lunch")) {
-                    //gets lunch
-                    lunch = s.substring(0, index2);
-                    //cuts breakfast and -
-                    s = s.substring(index2 + 1);
-                    //index of - after first time
-                    int indextemp = s.indexOf("-");
-                    //adding start time
-                    lunchhours += s.substring(0, indextemp);
-                    lunchhours += "-";
-                    s = s.substring(indextemp + 1);
-                    //getting index of dash after second time
-                    int indextemp2 = s.indexOf("-");
-                    //adding second time
-                    lunchhours += s.substring(0, indextemp2);
-                    s = s.substring(indextemp2 + 1);
-                    index2 = s.indexOf("-");
-                } else {
-                    //gets dinner
-                    dinner = s.substring(0, index2);
-                    //cuts dinner and -
-                    s = s.substring(index2 + 1);
-                    //index of - after first time
-                    int indextemp = s.indexOf("-");
-                    //adding start time
-                    //8
-                    dinnerhours += s.substring(0, indextemp);
-                    dinnerhours += "-";
-                    s = s.substring(indextemp + 1);
-                    //getting index of dash after second time
-                    int indextemp2 = s.indexOf("(");
-                    //adding second time
-                    dinnerhours += s.substring(0, indextemp2);
-                    s = s.substring(indextemp2 + 1);
-                    //causes while loop to stop
-                    index2 = -1;
-                    repeat = Integer.parseInt(s.substring(0, 1));
-                    System.out.println("Repeat: " + repeat);
-
-
-                }
-                System.out.println(breakfast);
-                System.out.println(breakfasthours);
-                System.out.println(lunch);
-                System.out.println(lunchhours);
-                System.out.println(dinner);
-                System.out.println(dinnerhours);
-
-            }
-        }
-
-
-    }
 }
