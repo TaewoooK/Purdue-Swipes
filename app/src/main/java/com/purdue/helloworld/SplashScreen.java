804cdb31d39f8a597dd661e53fe8bb8930491fb7
Tae Woo Kim <kim3434@purdue.edu>
2020-10-18 10:43:55 -0400

    loading screen

---
+++ app/src/main/java/com/purdue/helloworld/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.purdue.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int timeOut = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },timeOut);
+    }
+}
