9a191a520249b98cef2e8fea464e60290ff7eb01
Tae Woo Kim <kim3434@purdue.edu>
2020-10-17 20:11:00 -0400

    added jersey mikes, harrison grill, cosi

--- app/src/main/java/com/purdue/helloworld/ui/home/HomeFragment.java
+++ app/src/main/java/com/purdue/helloworld/ui/home/HomeFragment.java
@@ -58,13 +58,13 @@ ArrayList<Restaurant> restaurants = new ArrayList<>();
 
 */
 
-/*//
+/*
         Restaurant panera = new Restaurant("Panera Bread", "The company operates as Saint Louis Bread Company in the Greater St. Louis area, where it has over 100 locations. Offerings include bakery items, pasta, salads, sandwiches, soups, and specialty drinks.", "https://www.google.com/maps/place/Panera+Bread/@40.4228822,-86.9263825,15z/data=!4m8!1m2!2m1!1spanera!3m4!1s0x8812e37e1fd73ac5:0xc9d3acfdc4c66eb0!8m2!3d40.425488!4d-86.92268", getString(R.string.panera_time), "panera", true, false);
         Restaurant qdoba = new Restaurant("Qdoba", "Qdoba Mexican Eats is a chain of fast casual restaurants in the United States and Canada serving Mexican-style cuisine", "https://www.google.com/maps/place/QDOBA+Mexican+Eats/@40.4228502,-86.9263825,15z/data=!4m8!1m2!2m1!1sqdoba!3m4!1s0x8812e3cb41790565:0x16b270e6149d3109!8m2!3d40.4256215!4d-86.9224247", getString(R.string.qdoba_time), "qdoba", true, false);
-        Restaurant chickfila = new Restaurant("Chick Fil A", "Chick-fil-A is one of the largest American fast food restaurant chains and the largest whose specialty is chicken sandwiches", "https://www.google.com/maps/place/Chick-fil-A/@40.4229903,-86.9526471,13z/data=!4m8!1m2!2m1!1schick-fil-a!3m4!1s0x8812e322e59ee1ff:0x5a54ae845029c1e5!8m2!3d40.428475!4d-86.919535", getString(R.string.chickfila_time), "chickfila", true, false);
-        Restaurant amd = new Restaurant();
-        Restaurant amd = new Restaurant();
-        Restaurant amd = new Restaurant();
+        Restaurant chickFilA = new Restaurant("Chick Fil A", "Chick-fil-A is one of the largest American fast food restaurant chains and the largest whose specialty is chicken sandwiches", "https://www.google.com/maps/place/Chick-fil-A/@40.4229903,-86.9526471,13z/data=!4m8!1m2!2m1!1schick-fil-a!3m4!1s0x8812e322e59ee1ff:0x5a54ae845029c1e5!8m2!3d40.428475!4d-86.919535", getString(R.string.chickfila_time), "chickfila", true, false);
+        Restaurant jerseyMikes = new Restaurant("Jersey Mike's", "Jersey Mike's Subs is an American submarine sandwich chain headquartered in Wall Township, New Jersey. The Jersey Mike's franchise has almost 1,592 locations open and about 124 more in development across the United States, in addition to three locations in Queensland, Australia and two in Ontario, Canada", "https://www.google.com/maps/place/Purdue+University/@40.4285133,-86.9195129,21z/data=!4m5!3m4!1s0x8812fd37423e0507:0x8eccb2cf8b1a7c8e!8m2!3d40.4237054!4d-86.9211946", getString(R.string.jerseymikes_time), "jerseymikes", true, false);
+        Restaurant harrisonGrill = new Restaurant("Harrison Grill", "Dive into a new experience with american food and allow Harrison Grill to bring you something exquisite. We have options for every member of the family to explore! We are located in West Lafayette, feel free to explore our restaurant menu!", "https://www.google.com/maps/place/Harrison+Grill/@40.4249477,-86.9287688,17z/data=!3m1!4b1!4m5!3m4!1s0x8812e2c94783e7e1:0x7d7efb55ef3fb8c2!8m2!3d40.4249436!4d-86.9265801", getString(R.string.harrisongrill_time), "harrisongrill", true, false);
+        Restaurant cosi = new Restaurant("Cosi", "Così, based in Boston, Massachusetts, is an American fast-casual restaurant chain that is known for its homemade flatbread. The name comes from the opera Così fan tutte, which was a favorite of the original owner.", "https://www.google.com/maps/place/Purdue+University/@40.4276951,-86.9216452,17z/data=!4m5!3m4!1s0x8812fd37423e0507:0x8eccb2cf8b1a7c8e!8m2!3d40.4237054!4d-86.9211946", getString(R.string.cosi_time), "cosi", true, false);
         Restaurant amd = new Restaurant();
         Restaurant amd = new Restaurant();
         Restaurant amd = new Restaurant();
